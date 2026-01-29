147b2e214fca84d6098e694582df6380131b9f81
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-29 13:30:31 +0900

    Title: 정수 명령 처리 8, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260129/정수 명령 처리 8/process-numeric-commands-8.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            String command = sc.next();
+            // Please write your code here.
+            switch (command) {
+                case "push_front":
+                    int n = sc.nextInt();
+                    list.addFirst(n);
+                    break;
+                case "push_back":
+                    int n = sc.nextInt();
+                    list.addLast(n);
+                    break;
+                case "pop_front":
+                    System.out.println(list.pollFirst());
+                    break;
+                case "pop_back":
+                    System.out.println(list.pollLast());
+                    break;
+                case "size":
+                    System.out.println(list.size());
+                    break;
+                case "empty":
+                    System.out.println(list.isEmpty() ? 1 : 0);
+                    break;
+                case "front":
+                    System.out.println(list.peekFirst());
+                    break;
+                case "back":
+                    System.out.println(list.peekLast());
+                    break;
+            }
+        }
+    }
+}
