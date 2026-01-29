df7def519dcd960a67bfc13414b1c751372e55b4
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-01-29 16:49:39 +0900

    Title: 모이자, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260129/모이자/gather.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int min = 100 * 100;
+        for (int i = 0; i < n; i++) {
+            int distance = 0;
+            for (int k = 0; k < n; k++) {
+                distance += Math.abs(k - i) * a[k];
+            }
+            min = Integer.min(min, distance);
+        }
+        System.out.println(min);
+    }
+}
