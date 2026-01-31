333b999a307648b87fa8fb57c16627806257df8b
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-01 04:39:13 +0900

    Title: 기울어진 직사각형, Time: 151ms, Memory: 16MB, Status: Passed - Codetree

--- 260201/기울어진 직사각형/slanted-rectangle.java
+++ 260201/기울어진 직사각형/slanted-rectangle.java
@@ -12,45 +12,38 @@ public class Main {
         int answer = 0;
         for (int r = 0; r < n; r++) {
             for (int c = 0; c < n; c++) {
-                for (int k = 1; k < n; k++) {
-                    int sum = getSum(grid, r, c, k, n);
-                    answer = Integer.max(answer, sum);
+                // 직사각형의 가로, 세로 길이별 완전탐색
+                for (int w = 1; w < n; w++) {
+                    for (int h = 1; h < n; h++) {
+                        int sum = getSum(grid, r, c, w, h, n);
+                        answer = Integer.max(answer, sum);
+                    }
                 }
             }
         }
         System.out.println(answer);
     }
 
-    private static int getSum(int[][] grid, int x, int y, int k, int n) {
+    private static int getSum(int[][] grid, int x, int y, int w, int h, int n) {
         final int[][] dirs = {{-1, 1}, {-1, -1}, {1, -1}, {1, 1}};
-        boolean[] move = new boolean[4];
         int sum = 0;
         int cx = x;
         int cy = y;
         for (int i = 0; i < 4; i++) {
             int[] dir = dirs[i];
-            for (int j = 0; j < k; j++) {
-                if (cx == 0 && cy == n - 1) {
-                    return 0;
-                }
+            int count = i % 2 == 0 ? w : h;
+            for (int j = 0; j < count; j++) {
                 int nx = cx + dir[0];
                 int ny = cy + dir[1];
+                // 범위 벗어나는 경우엔 직사각형 만들 수 없음
                 if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
-                    break;
+                    return 0;
                 }
                 sum += grid[nx][ny];
-                move[i] = true;
-                if (nx == x && ny == y) {
-                    break;
-                }
                 cx = nx;
                 cy = ny;
             }
         }
-        // 안 움직인 방향이 있으면 직사각형 불가능
-        if (move[0] && move[1] && move[2] && move[3]) {
-            return sum;
-        }
-        return 0;
+        return sum;
     }
 }
