f3fec164be4fd23f144ee82a68c40e1153829b9f
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-03 15:58:21 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260203/떨어지는 1자 블록/falling-horizontal-block.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt() - 1;
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        int height = 0;
+        while (height < n) {
+            boolean canDown = true;
+            for (int c = k; c < k + m; c++) {
+                if (grid[height][c] != 0) {
+                    canDown = false;
+                    break;
+                }
+            }
+            if (!canDown) {
+                break;
+            }
+            ++
+        }
+
+        for (int c = k; c < k + m; c++) {
+            grid[height - 1][c] = 1;
+        }
+
+        for (int[] row : grid) {
+            for (int col : row) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+}
