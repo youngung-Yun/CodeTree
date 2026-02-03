8258985cc93b540216c6d8ffb9e80180142addc2
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-03 15:50:11 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 234ms, Memory: 17MB, Status: Passed - Codetree

--- 260203/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
+++ 260203/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
 
-    static int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0o}};
+    static int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
