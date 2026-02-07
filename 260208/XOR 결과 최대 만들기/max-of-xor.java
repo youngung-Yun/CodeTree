3a62d7f9c02b9792e735fbd698f4a512c238453d
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-08 02:28:41 +0900

    Title: XOR 결과 최대 만들기, Time: 92ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260208/XOR 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int m;
+    static int[] arr;
+
+    static int ans = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer token = new StringTokenizer(bf.readLine());
+
+        n = Integer.parseInt(token.nextToken());
+        m = Integer.parseInt(token.nextToken());
+
+        arr = new int[n];
+        token = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(token.nextToken());
+        }
+
+        dfs(0, 0, 0);
+        System.out.println(ans);
+    }
+
+    static void dfs(int curr, int depth, int start) {
+        if (depth == m) {
+            ans = Integer.max(ans, Math.abs(curr));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            if (depth == 0) {
+                dfs(i, depth + 1, i + 1);
+            } else {
+                dfs(curr ^ i, depth + 1, i + 1);
+            }
+        }
+    }
+}
