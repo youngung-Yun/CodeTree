178be8025be71c6cadb217ce4c05034a91ee81d3
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-08 02:57:38 +0900

    Title: 2N개 중에 N개의 숫자를 적절하게 고르기, Time: 92ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260208/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int[] arr;
+    static int ans = 0;
+
+    static boolean canReach = false;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(bf.readLine());
+        arr = new int[n*2];
+        StringTokenizer token = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < n * 2; i++) {
+            int element = Integer.parseInt(token.nextToken());
+            arr[i] = element;
+            ans += element;
+        }
+        dfs(0, 0, 0);
+        System.out.println(ans);
+    }
+
+    static void dfs(int groupA, int groupB, int depth) {
+        if (depth == n * 2) {
+            ans = Integer.min(ans, Math.abs(groupA - groupB));
+            return;
+        }
+
+        int curr = arr[depth];
+        dfs(groupA + curr, groupB, depth + 1);
+        dfs(groupA, groupB + curr, depth + 1);
+    }
+}
