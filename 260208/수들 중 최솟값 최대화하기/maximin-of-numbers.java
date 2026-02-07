de1d9c8f7a3ec4c344be288b740b8b3cb023dce7
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-08 03:51:29 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260208/수들 중 최솟값 최대화하기/maximin-of-numbers.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int n;
+    static int[][] grid;
+    static int ans = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(bf.readLine());
+        grid = new int[n][n];
+
+        for (int r = 0; r < n; r++) {
+            StringTokenizer token = new StringTokenizer(bf.readLine());
+            for (int c = 0; c < n; c++) {
+                grid[r][c] = Integer.parseInt(token.nextToken());
+            }
+        }
+
+        dfs(new boolean[n+1], 0, n * n);
+        System.out.println(ans);
+
+    }
+
+    static void dfs(boolean[] colVisited, int row, int min) {
+        if (row == n) {
+            ans = Integer.max(ans, min);
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (colVisited[col]) {
+                continue;
+            }
+
+            colVisited[col] = true;
+            dfs(colVisited, row + 1, Integer.min(min, grid[row][col]));
+            colVisited[col] = false;
+        }
+    }
+}
