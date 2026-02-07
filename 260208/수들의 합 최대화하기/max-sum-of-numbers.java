6965a6c0b8ac7b4cc58faeee05ed96c00ce20b71
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-08 03:31:47 +0900

    Title: 수들의 합 최대화하기, Time: 102ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260208/수들의 합 최대화하기/max-sum-of-numbers.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int n;
+    static int[][] grid;
+    static int ans = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(bf.readLine());
+        grid = new int[n][n];
+
+        for (int r = 0; r < n; r++) {
+            StringTokenizer token = new StringTokenizer(bf.readLine());
+            for (int c = 0; c < n; c++) {
+                grid[r][c] = Integer.parseInt(token.nextToken());
+            }
+        }
+
+        dfs(new boolean[n+1], 0, 0);
+        System.out.println(ans);
+
+    }
+
+    static void dfs(boolean[] colVisited, int row, int sum) {
+        if (row == n) {
+            ans = Integer.max(ans, sum);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (colVisited[col]) {
+                continue;
+            }
+
+            colVisited[col] = true;
+            dfs(colVisited, row + 1, sum + grid[row][col]);
+            colVisited[col] = false;
+        }
+    }
+}
