6850e1d17167092f95671689b63c72d586684263
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-09 22:44:57 +0900

    Title: 빙하, Time: 233ms, Memory: 15MB, Status: Passed - Codetree

--- 260209/빙하/glacier.java
+++ 260209/빙하/glacier.java
@@ -9,7 +9,6 @@ public class Main {
     static int n;
     static int m;
     static int[][] grid;
-    static List<int[]> starts = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -20,86 +19,70 @@ public class Main {
         grid = new int[n][m];
         for (int r = 0; r < n; r++) {
             stk = new StringTokenizer(bf.readLine());
-            for (int c = 0; c < n; c++) {
+            for (int c = 0; c < m; c++) {
                 grid[r][c] = Integer.parseInt(stk.nextToken());
             }
         }
 
-        findStarts();
         bfs();
-    }
-
-    private static void findStarts() {
-        Queue<int[]> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[n][m];
-        queue.offer(new int[] {0, 0});
-        visited[0][0] = true;
-
-        while (!queue.isEmpty()) {
-            int[] curr = queue.remove();
-            starts.add(curr);
-
-            for (int i = 0; i < 4; i++) {
-                int nx = curr[0] + dx[i];
-                int ny = curr[1] + dy[i];
-
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
-                    continue;
-                }
-                if (visited[nx][ny]) {
-                    continue;
-                }
-                if (grid[nx][ny] == 1) {
-                    continue;
-                }
-                queue.offer(new int[] {nx, ny});
-                visited[nx][ny] = true;
-            }
-        }
     }
 
     private static void bfs() {
         // [x, y, time]
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a1, a2) -> Integer.compare(a1[2], a2[2]));
-        boolean[][] visited = new boolean[n][m];
-        for (int[] start : starts) {
-            pq.add(new int[] {start[0], start[1], 0});
-            visited[start[0]][start[1]] = true;
+        Deque<int[]> pq = new ArrayDeque<>();
+        int[][] distance = new int[n][m];
+        for (int[] row : distance) {
+            Arrays.fill(row, -1);
         }
+        pq.offerLast(new int[] {0, 0});
+        distance[0][0] = 0;
 
         int maxTime = 0;
         int count = 0;
 
         while (!pq.isEmpty()) {
-            int[] curr = pq.remove();
+            int[] curr = pq.removeFirst();
 
-            if (grid[curr[0]][curr[1]] == 1) {
-                if (curr[2] > maxTime) {
-                    maxTime = curr[2];
-                    count = 1;
-                } else if (curr[2] == maxTime) {
-                    ++count;
-                }
-            }
-            
             for (int i = 0; i < 4; i++) {
                 int nx = curr[0] + dx[i];
                 int ny = curr[1] + dy[i];
+
                 if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
                     continue;
                 }
-                if (visited[nx][ny]) {
+                if (distance[nx][ny] != -1) {
                     continue;
                 }
                 if (grid[nx][ny] == 0) {
-                    visited[nx][ny] = true;
-                    pq.offer(new int[] {nx, ny, curr[2]});
+                    distance[nx][ny] = distance[curr[0]][curr[1]];
+                    pq.offerFirst(new int[] {nx, ny});
                 } else if (grid[nx][ny] == 1) {
-                    pq.offer(new int[] {nx, ny, curr[2] + 1});
-                    visited[nx][ny] = true;
+                    distance[nx][ny] = distance[curr[0]][curr[1]] + 1;
+                    pq.offerLast(new int[] {nx, ny});
                 }
             }
         }
-        System.out.println(maxTime + " " + count);
+
+        int[] result = findMax(distance);
+        System.out.println(result[0] + " " + result[1]);
+    }
+
+    private static int[] findMax(int[][] distance) {
+        int maxDistance = 0;
+        int count = 0;
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+                if (grid[r][c] == 0) {
+                    continue;
+                }
+                if (distance[r][c] > maxDistance) {
+                    maxDistance = distance[r][c];
+                    count = 1;
+                } else if (distance[r][c] == maxDistance) {
+                    ++count;
+                }
+            }
+        }
+        return new int[] {maxDistance, count};
     }
 }
