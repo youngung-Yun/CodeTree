32835dfa60ca3c134364c5b46c6342572ddc0589
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-10 09:42:46 +0900

    Title: 사각형 채우기, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 260210/사각형 채우기/rectangle-fill.java
+++ 260210/사각형 채우기/rectangle-fill.java
@@ -8,7 +8,7 @@ public class Main {
         dp[1] = 1;
         dp[2] = 2;
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] = (dp[i-1] + dp[i-2]) % 10_007;
         }
         System.out.println(dp[n]);
     }
