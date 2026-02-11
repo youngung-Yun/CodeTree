cbc600faee0c791161aa8457d7f8598302f3bc23
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-11 17:24:13 +0900

    Title: 최소 차 분할, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260211/최소 차 분할/minimum-diff-partition.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // dp[i][k] = i번째 수를 넣었을 때 최솟값 k를 만들 수 있는지
+        // == 최솟값 k를 만들 수 있으려면 i-1에서 k + number 또는 k - number가 true여야 함
+        boolean[][] dp = new boolean[n][1_001];
+        dp[0][arr[0]] = true;
+        for (int i = 1; i < n; i++) {
+            int number = arr[i];
+            for (int min = 0; min <= 1_000; min++) {
+                if (min + number < 1_000 && dp[i-1][min+number]) {
+                    dp[i][min] = true;
+                }
+                if (dp[i-1][Math.abs(min - number)]) {
+                    dp[i][min] = true;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int min = 0; min <= 1_000; min++) {
+            if (dp[n-1][min]) {
+                ans = min;
+                break;
+            }
+        }
+        System.out.println(ans);
+    }
+}
