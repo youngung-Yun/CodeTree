2a8ae8b9647640eaaa6c6416b56f95273dfc73a8
youngungyun <57798944+youngung-Yun@users.noreply.github.com>
2026-02-11 17:26:15 +0900

    Title: 최소 차 분할, Time: 171ms, Memory: 11MB, Status: Passed - Codetree

--- 260211/최소 차 분할/minimum-diff-partition.java
+++ 260211/최소 차 분할/minimum-diff-partition.java
@@ -9,14 +9,15 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
+        final int MAX = 1_000 * 100;
         // dp[i][k] = i번째 수를 넣었을 때 최솟값 k를 만들 수 있는지
-        // == 최솟값 k를 만들 수 있으려면 i-1에서 k + number 또는 k - number가 true여야 함
-        boolean[][] dp = new boolean[n][1_001];
+        // == 최솟값 k를 만들 수 있으려면 i-1에서 k + number 또는 Math.abs(k - number)가 true여야 함
+        boolean[][] dp = new boolean[n][MAX + 1];
         dp[0][arr[0]] = true;
         for (int i = 1; i < n; i++) {
             int number = arr[i];
-            for (int min = 0; min <= 1_000; min++) {
-                if (min + number < 1_000 && dp[i-1][min+number]) {
+            for (int min = 0; min <= MAX; min++) {
+                if (min + number < MAX && dp[i-1][min+number]) {
                     dp[i][min] = true;
                 }
                 if (dp[i-1][Math.abs(min - number)]) {
@@ -26,7 +27,7 @@ public class Main {
         }
 
         int ans = 0;
-        for (int min = 0; min <= 1_000; min++) {
+        for (int min = 0; min <= MAX; min++) {
             if (dp[n-1][min]) {
                 ans = min;
                 break;
